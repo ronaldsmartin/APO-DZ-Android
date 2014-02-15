5053b8ff3cb2cc1466c2cedef61edb30d3d1d7c8
Ronald Martin <ronaldsmartin@gmail.com>
2014-02-15 17:07:32 -0500

    Working embedded calendar - removed buttons for switching calendars.

--- APO-DZ/src/org/upennapo/app/MainActivity.java
+++ APO-DZ/src/org/upennapo/app/MainActivity.java
@@ -116,7 +116,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         		// Calendar WebView
         		Fragment webCalendar = new CalendarFragment();
         		Bundle urlArgs = new Bundle();
-        		urlArgs.putString(CalendarFragment.URL_KEY, "https://github.com");
+        		urlArgs.putString(CalendarFragment.URL_KEY, getString(R.string.calendar_url));
         		webCalendar.setArguments(urlArgs);
         		return webCalendar;
         	case 3:
@@ -166,21 +166,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
             return sectionName;
         }
     }
-    
-    public void openServiceCalendar(View v){
-    	CalendarFragment calFragment = (CalendarFragment) mSectionsPagerAdapter.getItem(1);
-    	calFragment.openServiceCalendar(v);
-    }
-    
-    public void openFellowshipCalendar(View v){
-    	CalendarFragment calFragment = (CalendarFragment) mSectionsPagerAdapter.getItem(1);
-    	calFragment.openFellowshipCalendar(v);
-    }
-    
-    public void openPledgingCalendar(View v){
-    	CalendarFragment calFragment = (CalendarFragment) mSectionsPagerAdapter.getItem(1);
-    	calFragment.openPledgingCalendar(v);
-    }
+
 
     /**
      * A dummy fragment representing a section of the app, but that simply
