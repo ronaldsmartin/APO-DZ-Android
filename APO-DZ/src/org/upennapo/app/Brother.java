c536919f331d9a6a07899e6ff379358676b402f9
Ronald Martin <ronaldsmartin@gmail.com>
2014-02-15 18:43:03 -0500

    Filled out the Brother object with directory keys and instance variables

--- APO-DZ/src/org/upennapo/app/Brother.java
+++ APO-DZ/src/org/upennapo/app/Brother.java
@@ -9,12 +9,30 @@ package org.upennapo.app;
  */
 public class Brother {
 	
-	// Name and Status
-	public String firstName;
+	// Keys
+	public static final String LAST_NAME_KEY = "Last_Name";
+	public static final String FIRST_NAME_KEY = "First_Name";
+	public static final String PREFERRED_NAME_KEY = "Preferred_Name";
+	public static final String PLEDGE_CLASS_KEY = "Pledge_Class";
+	public static final String EMAIL_ADDRESS_KEY = "E-Mail_Address";
+	public static final String PHONE_NUMBER_KEY = "Phone_Number";
+	public static final String GRADUATION_YEAR_KEY = "Expected_Graduation_Year";
+	public static final String SCHOOL_KEY = "School";
+	public static final String MAJOR_KEY = "Major";
+	public static final String BIRTHDAY_KEY = "Birthday";
+	
+	// Instance Variables
 	public String lastName;
+	public String firstName;
 
+	public String emailAddress;
+	public String phoneNumber;
 	
-
+	public String pledgeClass;
+	public int graduationYear;
 	
+	public String school;
+	public String major;
 	
+	public String birthday;
 }
