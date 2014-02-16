6e10f0bbe55e6b8cd45c698683f4a8ec91a3b68a
turniphead <darkwinter2424@gmail.com>
2014-02-16 08:21:49 -0500

    hi

--- APO-DZ/src/org/upennapo/app/LoginActivity.java
+++ APO-DZ/src/org/upennapo/app/LoginActivity.java
@@ -1,13 +1,31 @@
 package org.upennapo.app;
 
+import org.upennapo.app.DirectoryFragment.AsyncBrotherLoader;
+
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class LoginActivity extends Activity {
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_activity);
 		
+		SharedPreferences settings = getPreferences(0);
+//		if(settings.contains("username")){
+//			// go to status tab
+//		}
+//		else{
+//			AsyncBrotherLoader loader = new AsyncBrotherLoader();
+//			final String urlString = getString(R.string.directory_json_url);
+//			loader.execute(urlString);
+//			
+//			view = inflater.inflate(R.layout.fragment_directory, container, false);
+//
+//			
+//			return view;
+//		}
 		
 	}
 }
