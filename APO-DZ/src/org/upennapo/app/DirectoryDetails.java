0c6745d76933931ce765775c828b801d5123f61f
turniphead <darkwinter2424@gmail.com>
2014-02-16 07:33:54 -0500

    Phone dial!

--- APO-DZ/src/org/upennapo/app/DirectoryDetails.java
+++ APO-DZ/src/org/upennapo/app/DirectoryDetails.java
@@ -3,7 +3,9 @@ package org.upennapo.app;
 import java.util.HashMap;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -71,6 +73,21 @@ public class DirectoryDetails extends Activity {
 				emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, aEmailList);
 				emailIntent.setType("text/plain");
 				startActivity(emailIntent);
+			}
+		});
+		
+		Button call_b = (Button) findViewById(R.id.call_button);
+		call_b.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				try {
+			        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+			        callIntent.setData(Uri.parse("tel:" + number));
+			        startActivity(callIntent);
+			    } catch (ActivityNotFoundException e) {
+			         e.printStackTrace();
+			    }
 			}
 		});
 	}
