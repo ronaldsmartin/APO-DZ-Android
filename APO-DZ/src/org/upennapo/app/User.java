e39770f862546bfb0a59ac59c502804efa0d1018
Ronald Martin <ronaldsmartin@gmail.com>
2014-02-16 01:00:02 -0500

    Changed User object structure for easier paring by Gson

--- APO-DZ/src/org/upennapo/app/User.java
+++ APO-DZ/src/org/upennapo/app/User.java
@@ -1,32 +1,33 @@
 package org.upennapo.app;
 
 public class User {
+
 	// Name and Status
-	public String firstName;
-	public String lastName;
-	public String brotherStatus;
+	public String First_Name;
+	public String Last_Name;
+	public String Status;
 
 	// High-Level Completion
-	public boolean doneWithRequirements;
+	public boolean Complete;
 
 	// Service Details
-	public boolean doneWithService;
-	public float serviceHours;
-	public float reqServiceHours;
-	public boolean mandatoryServiceDone;
-	public boolean publicityDone;
-	public boolean serviceHostingDone;
+	public boolean Service;
+	public float Service_Hours;
+	public float Required_Service_Hours;
+	public boolean Large_Group_Project;
+	public boolean Publicity;
+	public boolean Service_Hosting;
 
 	// Fellowship Details
-	public boolean doneWithFellowship;
-	public float fellowshipPoints;
-	public float reqFellowshipPoints;
-	public boolean fellowshipHostingDone;
+	public boolean Fellowship;
+	public float Fellowship_Points;
+	public float Required_Fellowship;
+	public boolean Fellowship_Hosting;
 
 	// Membership Details
-	public boolean doneWithMembership;
-	public float membershipPoints;
-	public float reqMembershipPoints;
-	public boolean pledgeComponentDone;
-	public boolean brotherComponentDone;
+	public boolean Membership;
+	public float Membership_Points;
+	public float Required_Membership_Points;
+	public boolean Pledge_Comp;
+	public boolean Brother_Comp;
 }
