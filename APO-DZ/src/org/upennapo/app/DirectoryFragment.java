cc8f8a97f7e4d59b27bcc2090876b369ae22f1ad
turniphead <darkwinter2424@gmail.com>
2014-02-15 23:57:14 -0500

    shit works names are messed up

--- APO-DZ/src/org/upennapo/app/DirectoryFragment.java
+++ APO-DZ/src/org/upennapo/app/DirectoryFragment.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -21,6 +24,7 @@ import android.widget.ListView;
 public class DirectoryFragment extends Fragment{
 	
 	private List<Brother> directoryList;
+	private View view;
 	
 	public DirectoryFragment() {
     }
@@ -28,49 +32,32 @@ public class DirectoryFragment extends Fragment{
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
 		
-		View view = inflater.inflate(R.layout.fragment_directory, container, false);
-		
-
-		List<String> firstLast = new ArrayList<String>();
-		for (Brother obj: directoryList){
-			firstLast.add(obj.First_Name + " " + obj.Last_Name);
-		}
-		
-		// if this works
-		AlphabeticalAdapter adapt = new AlphabeticalAdapter(getActivity(), android.R.layout.simple_list_item_activated_1, firstLast);
-		
-		// if it doesn't work
-//		ArrayAdapter<String> adapt = new ArrayAdapter<String>(getActivity(),
-//                android.R.layout.simple_list_item_activated_1, firstLast);
-		
-		
-		ListView list = (ListView) view.findViewById(R.id.name_list);
-		list.setAdapter(adapt);
-		
-		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			public void onItemClick(AdapterView <?> parent, View view, int position, long id) {
-				Brother b = directoryList.get(position);
-				HashMap<String,String> broMap = new HashMap<String,String>();
-				broMap.put(Brother.LAST_NAME_KEY, b.Last_Name);
-				broMap.put(Brother.FIRST_NAME_KEY, b.First_Name);
-				broMap.put(Brother.EMAIL_ADDRESS_KEY, b.Email_Address);
-				broMap.put(Brother.PHONE_NUMBER_KEY, b.Phone_Number);
-				broMap.put(Brother.PLEDGE_CLASS_KEY, b.Pledge_Class);
-				broMap.put(Brother.GRADUATION_YEAR_KEY, String.valueOf(b.Expected_Graduation_Year));
-				broMap.put(Brother.SCHOOL_KEY, b.School);
-				broMap.put(Brother.MAJOR_KEY, b.Major);
-				broMap.put(Brother.BIRTHDAY_KEY, b.Birthday);
-				
-				Intent broPage = new Intent(getActivity(), DirectoryDetails.class);
-				broPage.putExtra(getString(R.string.dir_brother_data), broMap);
-				getActivity().startActivity(broPage);
-			}
-			
-		});
-		
 		AsyncBrotherLoader loader = new AsyncBrotherLoader();
 		final String urlString = getString(R.string.directory_json_url);
 		loader.execute(urlString);
+		
+		view = inflater.inflate(R.layout.fragment_directory, container, false);
+		
+//		try{
+//			loader.get(10000, TimeUnit.MILLISECONDS);
+//		}
+//		catch(TimeoutException e){
+//			//e.printStackTrace();
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			//e.printStackTrace();
+//		} catch (ExecutionException e) {
+//			// TODO Auto-generated catch block
+//			//e.printStackTrace();
+//		}
+
+		
+		
+		
+		
+		
+		
+
 		
 		return view;
 	}
@@ -93,6 +80,45 @@ public class DirectoryFragment extends Fragment{
 	        for (Brother brother : directoryList) {
 	        	Log.d("brotherData", brother.toString());
 	        }
+	        
+	        
+	        List<String> firstLast = new ArrayList<String>();
+			for (Brother obj: directoryList){
+				firstLast.add(obj.First_Name + " " + obj.Last_Name);
+			}
+			
+			// if this works
+			AlphabeticalAdapter adapt = new AlphabeticalAdapter(getActivity(), android.R.layout.simple_list_item_activated_1, firstLast);
+			
+			// if it doesn't work
+//			ArrayAdapter<String> adapt = new ArrayAdapter<String>(getActivity(),
+//	                android.R.layout.simple_list_item_activated_1, firstLast);
+			
+			
+			ListView list = (ListView) view.findViewById(R.id.name_list);
+			list.setAdapter(adapt);
+			
+			list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+				public void onItemClick(AdapterView <?> parent, View view, int position, long id) {
+					Brother b = directoryList.get(position);
+					HashMap<String,String> broMap = new HashMap<String,String>();
+					broMap.put(Brother.LAST_NAME_KEY, b.Last_Name);
+					broMap.put(Brother.FIRST_NAME_KEY, b.First_Name);
+					broMap.put(Brother.EMAIL_ADDRESS_KEY, b.Email_Address);
+					broMap.put(Brother.PHONE_NUMBER_KEY, b.Phone_Number);
+					broMap.put(Brother.PLEDGE_CLASS_KEY, b.Pledge_Class);
+					broMap.put(Brother.GRADUATION_YEAR_KEY, String.valueOf(b.Expected_Graduation_Year));
+					broMap.put(Brother.SCHOOL_KEY, b.School);
+					broMap.put(Brother.MAJOR_KEY, b.Major);
+					broMap.put(Brother.BIRTHDAY_KEY, b.Birthday);
+					
+					Intent broPage = new Intent(getActivity(), DirectoryDetails.class);
+					broPage.putExtra(getString(R.string.dir_brother_data), broMap);
+					getActivity().startActivity(broPage);
+				}
+				
+			});
+	        
 	    }
 	    
 	}
