80d53b69388b40b78e6d88d64dd84538ec0a7657
Ronald Martin <ronaldsmartin@gmail.com>
2014-03-28 16:49:56 -0400

    Make login screen default Activity.

--- APO-DZ/src/org/upennapo/app/LoginActivity.java
+++ APO-DZ/src/org/upennapo/app/LoginActivity.java
@@ -1,8 +1,10 @@
 package org.upennapo.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends Activity {
 	
@@ -10,7 +12,7 @@ public class LoginActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_activity);
 		
-		SharedPreferences settings = getPreferences(0);
+		// SharedPreferences settings = getPreferences(0);
 //		if(settings.contains("username")){
 //			// go to status tab
 //		}
@@ -25,5 +27,14 @@ public class LoginActivity extends Activity {
 //			return view;
 //		}
 		
+	}
+	
+	/**
+	 * Opened when clicking button in the LoginView
+	 * @param view - the button in login_activity.xml
+	 */
+	public void login(View view) {
+		Intent openAppIntent = new Intent(this, MainActivity.class);
+		startActivity(openAppIntent);
 	}
 }
