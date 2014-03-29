226299b7a6879fcaeef61730f06a6fec61826445
Ronald Martin <ronaldsmartin@gmail.com>
2014-03-29 02:11:02 -0400

    Now using url argument passing so we can reuse the directory class

--- APO-DZ/src/org/upennapo/app/DirectoryFragment.java
+++ APO-DZ/src/org/upennapo/app/DirectoryFragment.java
@@ -5,45 +5,42 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.Comparator;
 
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class DirectoryFragment extends Fragment{
 	
+	public static final String URL_KEY   = "URL";
+	public static final String SHEET_KEY = "SHEET_KEY";
 	private List<Brother> directoryList;
 	private View view;
 	
 	public DirectoryFragment() {
     }
+
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
+		// Retrieve the argumentS passed by the MainActivity
+        final String urlString = getArguments().getString(URL_KEY);
+        final String sheetKey  = getArguments().getString(SHEET_KEY);
 		
+		// Make an asynchronous request for the JSON using the URL
 		AsyncBrotherLoader loader = new AsyncBrotherLoader();
-		final String urlString = getString(R.string.directory_json_url);
-		loader.execute(urlString);
+		loader.execute(urlString, sheetKey);
 		
+		// Inflate the View
 		view = inflater.inflate(R.layout.fragment_directory, container, false);
-
-		
 		return view;
 	}
 	
@@ -56,7 +53,7 @@ public class DirectoryFragment extends Fragment{
 
 	    @Override
 	    protected Brother[] doInBackground(String... params) {
-	    	return ReadJSON.getDirectoryData(params[0]);
+	    	return ReadJSON.getDirectoryData(params[0], params[1]);
 	    }
 	    
 	    @Override
@@ -70,25 +67,16 @@ public class DirectoryFragment extends Fragment{
 					brother.Preferred_Name.equals("") ? brother.First_Name : brother.Preferred_Name;
 				firstLast.add(firstName + " " + brother.Last_Name);
 			}
-			// Collections.sort(firstLast);
-			
-			// if this works
-			AlphabeticalAdapter adapt =
+
+			AlphabeticalAdapter adapter =
 				new AlphabeticalAdapter(getActivity(), R.layout.centered_textview, R.id.centered_text, firstLast);
-			
-			// if it doesn't work
-//			ArrayAdapter<String> adapt = new ArrayAdapter<String>(getActivity(),
-//	                android.R.layout.simple_list_item_activated_1, firstLast);
-			
-//			ImageView im  = (ImageView) view.findViewById(R.id.viet);
-//			TextView t = (TextView) view.findViewById(R.id.text);
 			ListView list = (ListView) view.findViewById(R.id.name_list);
 			
-			//t.setText("");
-			list.setAdapter(adapt);
+			list.setAdapter(adapter);
 			
 			list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 				public void onItemClick(AdapterView <?> parent, View view, int position, long id) {
+					// Prepare data to send to the details view.
 					Brother b = directoryList.get(position);
 					HashMap<String,String> broMap = new HashMap<String,String>();
 					broMap.put(Brother.LAST_NAME_KEY, b.Last_Name);
@@ -102,9 +90,10 @@ public class DirectoryFragment extends Fragment{
 					broMap.put(Brother.MAJOR_KEY, b.Major);
 					broMap.put(Brother.BIRTHDAY_KEY, b.Birthday);
 					
-					Intent broPage = new Intent(getActivity(), DirectoryDetails.class);
-					broPage.putExtra(getString(R.string.dir_brother_data), broMap);
-					getActivity().startActivity(broPage);
+					// Open the details view.
+					Intent detailPage = new Intent(getActivity(), DirectoryDetails.class);
+					detailPage.putExtra(getString(R.string.dir_brother_data), broMap);
+					getActivity().startActivity(detailPage);
 				}
 				
 			});
@@ -115,14 +104,15 @@ public class DirectoryFragment extends Fragment{
 	public class BrotherComparator implements Comparator<Brother> {
 		@Override
 		public int compare(Brother b1, Brother b2) {
-			String preferredName1 = b1.Preferred_Name;
-			String firstName1;
-			if (preferredName1.length() == 0) firstName1 = b1.First_Name;
-			else firstName1 = preferredName1;
-			String preferredName2 = b2.Preferred_Name;
-			String firstName2;
-			if (preferredName2.length() == 0) firstName2 = b2.First_Name;
-			else firstName2 = preferredName2;
+			// Retrieve brother 1's first name.
+			final String preferredName1 = b1.Preferred_Name;
+			final String firstName1 = preferredName1.length() == 0 ? b1.First_Name : preferredName1;
+			
+			// Retrieve brother 2's first name.
+			final String preferredName2 = b2.Preferred_Name;
+			final String firstName2 = preferredName2.length() == 0 ? b2.First_Name : preferredName2;
+			
+			// Compare and return lexographic ordering.
 			return firstName1.compareToIgnoreCase(firstName2);
 		}
 	}
