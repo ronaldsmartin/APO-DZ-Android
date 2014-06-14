34ac6346c997aadcfe85c253f0224c9150d2cd3c
Ronald Martin <ronaldsmartin@gmail.com>
2014-06-13 23:31:42 -0700

    Refactor CalendarFragment layout

--- APO-DZ/src/org/upennapo/app/CalendarFragment.java
+++ APO-DZ/src/org/upennapo/app/CalendarFragment.java
@@ -3,9 +3,9 @@
  */
 package org.upennapo.app;
 
-import android.support.v4.app.Fragment;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,18 +20,17 @@ public class CalendarFragment extends Fragment {
 	public static final String URL_KEY = "url";
 	
 	public CalendarFragment() {
-		
 	}
 
     @SuppressLint("SetJavaScriptEnabled")
 	@Override
     public View onCreateView(LayoutInflater inflater,
     			ViewGroup container, Bundle savedInstanceState) {
-        View calendarView = inflater.inflate(R.layout.fragment_calendar_tab, container, false);
+        View calendarView = inflater.inflate(R.layout.fragment_webview, container, false);
         
         final String urlToLoad = getArguments().getString(URL_KEY);
         if (urlToLoad != null) {
-            WebView calendarWebView = (WebView) calendarView.findViewById(R.id.calendar_webview);
+            WebView calendarWebView = (WebView) calendarView.findViewById(R.id.webView);
             calendarWebView.getSettings().setJavaScriptEnabled(true);
             calendarWebView.loadUrl(urlToLoad);
         }
