53b6e259ec649ef75f853cdf4878dcca87a199c0
Ronald Martin <ronaldsmartin@gmail.com>
2014-07-05 00:58:54 -0700

    AlumniMode: enable 2048 by default

--- APO-DZ/src/org/upennapo/app/AlumniModeActivity.java
+++ APO-DZ/src/org/upennapo/app/AlumniModeActivity.java
@@ -60,11 +60,11 @@ public class AlumniModeActivity extends FragmentActivity implements ActionBar.On
                 getActionBar().getSelectedNavigationIndex());
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        menu.findItem(R.id.menu_play_2048).setVisible(true);
         return true;
     }
 
