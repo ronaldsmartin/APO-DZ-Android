b1627cdc3472aafe55c3ec19587b285f0ce8a5e4
Ronald Martin <ronaldsmartin@gmail.com>
2014-08-21 00:58:03 -0700

    Upgrade QuickReturnBar to makovkastar's FloatingActionButton implementation; add ButterKnife

--- APO-DZ/src/org/upennapo/app/fragment/AlumDirectoryFragment.java
+++ APO-DZ/src/org/upennapo/app/fragment/AlumDirectoryFragment.java
@@ -1,16 +1,14 @@
 package org.upennapo.app.fragment;
 
-import android.animation.Animator;
-import android.animation.ObjectAnimator;
+import android.app.ActionBar;
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.Button;
 import android.widget.Toast;
+
+import com.melnykov.fab.FloatingActionButton;
 
 import org.upennapo.app.R;
 import org.upennapo.app.model.Brother;
@@ -19,52 +17,30 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
 
 /**
- * An extension of DirectoryFragment that loads directory from all directory sources, using
- * ActionItems to toggle which source is displayed.
- * <p/>
- * QuickReturnBar code is based on the work of Dandré Allison:
- * https://gist.github.com/imminent/4576886
+ * An extension of {@link DirectoryFragment} that loads directory from all directory sources, using
+ * a FloatingActionButton to toggle which source is displayed.
  *
  * @author Ronald Martin
  */
-public class AlumDirectoryFragment extends DirectoryFragment implements View.OnClickListener,
-        AbsListView.OnScrollListener {
+public class AlumDirectoryFragment extends DirectoryFragment {
 
     /* Tag for debugging */
     public static final String TAG = "AlumDirectoryFragment";
     /* Keys allow storage and retrieval of data during Fragment lifecycle. */
     private static final String LIST_BROS = "LIST_BROS";
     private static final String LIST_ALUM = "LIST_ALUM";
-
+    @InjectView(R.id.button_floating_action)
+    FloatingActionButton mToggleDirectoryBtn;
     /* Store entries independently of directory list to allow toggling by user. */
     private ArrayList<Brother> mStudentList, mAlumList;
-    /* Toggle buttons for switching directory lists. */
-    private Button mShowAlumBtn, mShowStudentBtn;
 
-    //region QuickReturnBarState
-    /**
-     * The current state of the quick return bar
-     */
-    private QuickReturnState mQuickReturnState = QuickReturnState.ON_SCREEN;
-    /**
-     * Index of the last seen first visible child
-     */
-    private int mLastFirstVisibleItemIndex;
-    /**
-     * Tracks the last seen y-position of the first visible child
-     */
-    private int mLastFirstVisibleChildYPos;
-    /**
-     * Animates the quick return bar off of the screen
-     */
-    private ObjectAnimator mQuickReturnBarReturnAnimator;
-    /**
-     * Animates the quick return bar onto the screen
-     */
-    private ObjectAnimator mQuickReturnBarHideAnimator;
-    //endregion
+    //region Fragment lifecycle
 
     /**
      * Returns new instance of AlumDirectoryFragment with arguments set.
@@ -107,21 +83,18 @@ public class AlumDirectoryFragment extends DirectoryFragment implements View.OnC
     @Override
     protected void init(Bundle savedInstanceState, View view) {
         super.init(savedInstanceState, view);
-        initQuickReturnBar(view);
-        initButtons(view);
+        ButterKnife.inject(this, view);
+        mToggleDirectoryBtn.attachToListView(getListView());
+        getActionBar().setSubtitle(R.string.alumni);
     }
 
-    /**
-     * Attach the buttons.
-     *
-     * @param rootView the root view inflated by the fragment.
-     */
-    private void initButtons(View rootView) {
-        mShowAlumBtn = (Button) rootView.findViewById(R.id.btn_show_alum);
-        mShowStudentBtn = (Button) rootView.findViewById(R.id.btn_show_students);
+    @Override
+    public void onResume() {
+        super.onResume();
 
-        mShowAlumBtn.setOnClickListener(this);
-        mShowStudentBtn.setOnClickListener(this);
+        // Set the appropriate ActionBar subtitle text.
+        if (isShowingAlumDirectory()) getActionBar().setSubtitle(R.string.students);
+        else if (isShowingStudentDirectory()) getActionBar().setSubtitle(R.string.alumni);
     }
 
     @Override
@@ -130,6 +103,19 @@ public class AlumDirectoryFragment extends DirectoryFragment implements View.OnC
         outState.putParcelableArrayList(LIST_BROS, mStudentList);
         outState.putParcelableArrayList(LIST_ALUM, mAlumList);
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActionBar().setSubtitle(null);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+    //endregion
 
     @Override
     protected void loadData(final boolean forceDownload) {
@@ -174,6 +160,10 @@ public class AlumDirectoryFragment extends DirectoryFragment implements View.OnC
         }.execute(sheetKey, "" + forceDownload);
     }
 
+    protected ActionBar getActionBar() {
+        return getActivity().getActionBar();
+    }
+
     @Override
     protected void updateListView() {
         super.updateListView();
@@ -187,213 +177,29 @@ public class AlumDirectoryFragment extends DirectoryFragment implements View.OnC
         }
     }
 
-    @Override
-    public void onClick(View view) {
-        // Refresh the index for the QuickReturnBar
-        mLastFirstVisibleItemIndex = getListView().getFirstVisiblePosition();
-        final View firstItem = getListView().getChildAt(mLastFirstVisibleItemIndex);
-        mLastFirstVisibleChildYPos = firstItem == null ? 0 : firstItem.getTop();
-
-        switch (view.getId()) {
-            case R.id.btn_show_alum:
-                switchToAlumList();
-                break;
-            case R.id.btn_show_students:
-                switchToStudentList();
-                break;
-        }
+    private boolean isShowingAlumDirectory() {
+        return getAdapter() != null && mAlumList != null
+                && getAdapter().getCount() == mAlumList.size();
     }
 
-    /**
-     * If we are currently displaying the student directory, switch to show the alumni directory and
-     * change button colors accordingly.
-     */
-    private void switchToAlumList() {
-        if (getAdapter() != null && getAdapter().getCount() != mAlumList.size()) {
-            setDirectoryList(mAlumList);
-            updateListView();
-            mShowAlumBtn.setTextColor(getResources().getColor(R.color.accent_fallback_light));
-            mShowStudentBtn.setTextColor(getResources().getColor(android.R.color.white));
-        }
+    private boolean isShowingStudentDirectory() {
+        return getAdapter() != null && mStudentList != null
+                && getAdapter().getCount() == mStudentList.size();
     }
 
+    @SuppressWarnings("unused")
+    @OnClick(R.id.button_floating_action)
     /**
-     * If we are currently displaying the alumni directory, switch to show the student directory and
-     * change button colors accordingly.
+     * Switch which directory the adapter is currently showing.
      */
-    private void switchToStudentList() {
-        if (getAdapter() != null && getAdapter().getCount() != mStudentList.size()) {
+    public void toggleDirectoryList() {
+        if (isShowingAlumDirectory()) {
             setDirectoryList(mStudentList);
-            updateListView();
-            mShowStudentBtn.setTextColor(getResources().getColor(R.color.accent_fallback_light));
-            mShowAlumBtn.setTextColor(getResources().getColor(android.R.color.white));
+            getActionBar().setSubtitle(R.string.students);
+        } else if (isShowingStudentDirectory()) {
+            setDirectoryList(mAlumList);
+            getActionBar().setSubtitle(R.string.alumni);
         }
+        updateListView();
     }
-
-    //region QuickReturnBar
-
-    /**
-     * Set up the Quick Return Bar's animations.
-     *
-     * @param rootView the view holding the quick return bar
-     */
-    private void initQuickReturnBar(View rootView) {
-        final AbsListView listView = (AbsListView) rootView.findViewById(R.id.name_list);
-
-        // Record the index and y-position of the first visible child.
-        mLastFirstVisibleItemIndex = listView.getFirstVisiblePosition();
-        final View firstItem = listView.getChildAt(mLastFirstVisibleItemIndex);
-        mLastFirstVisibleChildYPos = firstItem == null ? 0 : firstItem.getTop();
-
-        // Set up quick return animations.
-        final View quickReturnBar = rootView.findViewById(R.id.quick_return_bar);
-        final String animationPropertyName = "translationY";
-
-        mQuickReturnBarHideAnimator =
-                ObjectAnimator.ofFloat(quickReturnBar, animationPropertyName, 0)
-                        .setDuration(210);
-
-        mQuickReturnBarHideAnimator.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) {
-                mQuickReturnState = QuickReturnState.RETURNING;
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                mQuickReturnState = QuickReturnState.ON_SCREEN;
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-                mQuickReturnState = QuickReturnState.OFF_SCREEN;
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-            }
-        });
-
-        mQuickReturnBarReturnAnimator =
-                ObjectAnimator.ofFloat(quickReturnBar,
-                        animationPropertyName,
-                        getResources().getDimension(R.dimen.quick_return_bar_height))
-                        .setDuration(128);
-        mQuickReturnBarReturnAnimator.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) {
-                mQuickReturnState = QuickReturnState.HIDING;
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                mQuickReturnState = QuickReturnState.OFF_SCREEN;
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-                mQuickReturnState = QuickReturnState.ON_SCREEN;
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-            }
-        });
-
-        listView.setOnScrollListener(this);
-    }
-
-    /**
-     * Checks if the quick return bar is transitioning back onto the screen.
-     *
-     * @return {@code true} indicates that the quick return bar is returning
-     */
-    private boolean quickReturnBarIsReturning() {
-        return (mQuickReturnBarHideAnimator.isRunning()
-                || mQuickReturnBarHideAnimator.isStarted());
-    }
-
-    /**
-     * Checks if the quick return bar is transitioning off of the screen.
-     *
-     * @return {@code true} indicates that the quick return bar is going away
-     */
-    private boolean quickReturnBarIsGoingAway() {
-        return mQuickReturnBarReturnAnimator.isRunning()
-                || mQuickReturnBarReturnAnimator.isStarted();
-    }
-
-    @Override
-    public void onScroll(AbsListView listView, int firstVisibleItem, int visibleItemCount,
-                         int totalItemCount) {
-        final View firstChild = listView.getChildAt(firstVisibleItem);
-        final int firstChildY = firstChild == null ? 0 : firstChild.getTop();
-
-        switch (mQuickReturnState) {
-            case OFF_SCREEN:
-                if (!quickReturnBarIsReturning()
-                        && (firstVisibleItem == mLastFirstVisibleItemIndex && firstChildY > mLastFirstVisibleChildYPos)
-                        || firstVisibleItem < mLastFirstVisibleItemIndex) {
-                    mQuickReturnBarHideAnimator.start();
-                    Log.d(TAG, "Hide animation started.");
-                }
-                break;
-
-            case ON_SCREEN:
-                if (!quickReturnBarIsGoingAway()
-                        && (firstVisibleItem == mLastFirstVisibleItemIndex && firstChildY < mLastFirstVisibleChildYPos)
-                        || firstVisibleItem > mLastFirstVisibleItemIndex) {
-                    mQuickReturnBarReturnAnimator.start();
-                    Log.d(TAG, "Return animation started.");
-                }
-                break;
-
-            case RETURNING:
-                if ((firstVisibleItem == mLastFirstVisibleItemIndex && firstChildY < mLastFirstVisibleChildYPos)
-                        || firstVisibleItem > mLastFirstVisibleItemIndex) {
-                    mQuickReturnBarHideAnimator.cancel();
-                    mQuickReturnBarReturnAnimator.start();
-                    Log.d(TAG, "Return cancelled, hide animation started.");
-                }
-                break;
-
-            case HIDING:
-                if ((firstVisibleItem == mLastFirstVisibleItemIndex && firstChildY > mLastFirstVisibleChildYPos)
-                        || firstVisibleItem < mLastFirstVisibleItemIndex) {
-                    mQuickReturnBarReturnAnimator.cancel();
-                    mQuickReturnBarHideAnimator.start();
-                    Log.d(TAG, "Hide cancelled, return animation started.");
-                }
-                break;
-        }
-        mLastFirstVisibleItemIndex = firstVisibleItem;
-        mLastFirstVisibleChildYPos = firstChildY;
-    }
-
-    @Override
-    public void onScrollStateChanged(AbsListView listView, int scrollState) {
-    }
-
-    /**
-     * Quick Return Bar **
-     */
-    private static enum QuickReturnState {
-        /**
-         * Stable state indicating that the quick return bar is visible on screen
-         */
-        ON_SCREEN,
-        /**
-         * Stable state indicating that the quick return bar is hidden off screen
-         */
-        OFF_SCREEN,
-        /**
-         * Transitive state indicating that the quick return bar is coming onto the screen
-         */
-        RETURNING,
-        /**
-         * Transitive state indicating that the quick return bar is going off of the screen
-         */
-        HIDING
-    }
-    //endregion
 }
